141cb01c53e4df603d1e02753965e76c64526511
Maciej Rybarczyk <maciej.rybarczyk@onet.pl>
2025-01-05 14:21:53 +0100

    Add more sidebar buttons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -32,10 +32,130 @@
             </a>
           </li>
           <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
+                </svg>
+              </span>
+              <span class="ml-3">Schedule</span>
+            </a>
+          </li>
+          <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+                </svg>
+              </span>
+              <span class="ml-3">Subscriptions</span>
+            </a>
+          </li>
+          <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
+                </svg>
+              </span>
+              <span class="ml-3">Gyms</span>
+            </a>
+          </li>
+          <li class="my-px">
             <span class="flex font-bold text-sm text-white px-3 my-4 uppercase">Receptionist</span>
           </li>
           <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                </svg>
+              </span>
+              <span class="ml-3">Clients</span>
+            </a>
+          </li>
+          <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                          d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+              </span>
+              <span class="ml-3">Check membership</span>
+            </a>
+          </li>
+          <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                </svg>
+              </span>
+              <span class="ml-3">Store</span>
+            </a>
+          </li>
+          <li class="my-px">
             <span class="flex font-bold text-sm text-white px-3 my-4 uppercase">Coach</span>
+          </li>
+          <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+                </svg>
+              </span>
+              <span class="ml-3">Classes</span>
+            </a>
+          </li>
+          <li class="my-px">
+            <a
+              href="#"
+              class="flex flex-row items-center h-10 px-3 rounded-lg text-white hover:bg-orange-600 hover:font-semibold"
+            >
+              <span class="flex items-center justify-center text-lg text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                  <path stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                </svg>
+              </span>
+              <span class="ml-3">Participants</span>
+            </a>
           </li>
         </ul>
       </div>
